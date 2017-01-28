7062b1327d25db022cef0372a507c4f1c6e51f71
AmilaVM <amilalunuwila@gmail.com>
2017-01-28 14:54:52 +0530

    added Profile model

---
+++ src/main/java/com/jkcs/profilebuilder/repository/UserProfileRepository.java
@@ -0,0 +1,15 @@
+package com.jkcs.profilebuilder.repository;
+
+
+import com.jkcs.profilebuilder.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Created by AmilaVM on 1/26/2017.
+ */
+public interface UserProfileRepository extends JpaRepository<UserProfile, Integer> {
+
+
+}
